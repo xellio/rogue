57017de98de342b0a903b3ddfe82d78f0987ee9a
Richard Prillwitz <richard@ripr.io>
2017-02-09 09:03:33 +0100

    initial commit

---
+++ include/rogue.h
@@ -0,0 +1,106 @@
+#ifndef ROGUE_H
+#define ROGUE_H
+
+#include <curses.h>
+#include <stdlib.h>
+#include <time.h>
+
+/****** Struct Definitions ******/
+
+typedef struct Level {
+	char ** tiles;
+	int level;
+	int numberOfRooms;
+	struct Room ** rooms;
+	struct Monster ** monsters;
+	int numberOfMonsters;
+	struct Player * user;
+} Level;
+
+typedef struct Position {
+	int x;
+	int y;
+	//TILE_TYPE tile;
+} Position;
+
+typedef struct Room {
+	Position position;
+	int height;
+	int width;
+
+	struct Door ** doors;
+	int numberOfDoors;
+	//Monster ** monsters;
+	//Item ** items;
+} Room;
+
+typedef struct Door {
+	Position position;
+	int connected;
+
+} Door;
+
+typedef struct Player {
+	Position * position;
+	int health;
+	int attack;
+	int gold;
+	int maxHealth;
+	int exp;
+	//Room * room;
+} Player;
+
+typedef struct Monster {
+	char string[2];
+	char symbol;
+	int health;
+	int attack;
+	int speed;
+	int defence;
+	int pathfinding;
+	int alive;
+	Position * position;
+} Monster;
+
+/****** Global Variables ******/
+int MAX_HEIGHT;
+int MAX_WIDTH;
+
+
+/* screen functions */
+int screenSetUp();
+int printGameHub(Level * level);
+
+/* level/map functions */
+Level * createLevel(int level);
+Room ** roomsSetUp();
+char ** saveLevelPositions();
+void connectDoors(Level * level);
+
+/* player functions */ 
+Player *  playerSetUp();
+Position * handleInput(int input, Player * user);
+int checkPosition(Position * newPosition, Level * level);
+int playerMove(Position * newPosition, Player * user, char ** level);
+int placePlayer(Room ** rooms, Player * user);
+
+/* room functions */
+Room * createRoom(int grid, int numberOfDoors);
+int drawRoom(Room * room);
+
+/* monster functions */
+int addMonsters(Level * level);
+Monster * selectMonster(int level);
+Monster * createMonster(char symbol, int health, int attack, int speed, int defence, int pathfinding);
+int killMonster(Monster * monster);
+int setStartingPosition(Monster * monster, Room * room);
+int moveMonsters(Level * level);
+int pathfindingSeek(Position * start, Position * destination);
+int pathfindingRandom(Position * position);
+Monster * getMonsterAt(Position * position, Monster ** monsters);
+
+/* combat functions */
+int combat(Player * player, Monster * monster, int order);
+
+
+#endif
---
+++ makefile
@@ -0,0 +1,21 @@
+CC = gcc
+CFLAGS = -lmenu -lncurses -I$(IDIR)
+
+IDIR = ./include/
+
+SRCDIR = ./src/
+
+SOURCES = $(SRCDIR)*.c\
+		  $(SRCDIR)windows/*.c\
+		  $(SRCDIR)utils/*.c
+
+all: rogue
+
+rogue: $(SOURCES)
+	$(CC) $(SOURCES) $(CFLAGS) -o $@
+
+run:
+	./rogue
+
+clean:
+	rm -f rogue
